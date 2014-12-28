d84dc0efb802a2f551177a1c4df913c3f96bd5d3
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-12-28 20:42:06 +0100

    more code

--- main.c
+++ main.c
@@ -1,3 +1,12 @@
 // http://nullprogram.com/blog/2014/12/23/
 
 #include "api.h"
+
+const char *RUN_LIBRARY = "./librun.so";
+
+struct run {
+  void *handle;
+  ino_t id;
+  struct run_api api;
+  struct run_state *state;
+};
--- run.c
+++ run.c
@@ -1,4 +1,7 @@
 #include "api.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 const struct run_api RUN_API = {
   .init = init,
@@ -7,4 +10,25 @@ const struct run_api RUN_API = {
   .unload = unload,
   .step = step
 };
+
+void reload_if_new_lib(struct run*run)
+{
+  struct stat attr;
+  if(stat(GAME_LIBRARY,&attr)!=0){
+    printf("game library doesn't exist.\n");
+    return;
+  }
+  if (game->id == attr.st_ino) { // note: id is initially 0 and the
+				 // file should therefore be loaded at
+				 // first call
+    printf("file didn't change\n");
+    return;
+  }
+  if(run->handle){
+    run->api.unload(run->state);
+    dlclose(run->handle);
+  }
+  void *handle = dlopen(GAME_LIBRARY,RTLD_NOW);
   
+}
+
