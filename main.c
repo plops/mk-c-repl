3e6326ebe745ceb69e804872baa74e1a1c439207
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-12-31 16:41:42 +0100

    the error was the function names in run.c. they must not be init finalize and so on but r_init ..

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 // http://nullprogram.com/blog/2014/12/23/
 
-#include "api.h"
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdio.h>
- #include <dlfcn.h>
+#include <dlfcn.h>
+#include "api.h"
 
 const char *RUN_LIBRARY = "./librun.so";
+
+#define d(e) do{if(0)(e);}while(0)
 
 struct run {
   void *handle;
@@ -19,63 +22,132 @@ struct run {
 static void run_unload(struct run*run)
 {
   if(run->handle){
-    run->api.unload(run->state);
+    run->api.finalize(run->state);
+    run->state = NULL;
     dlclose(run->handle);
+    run->handle = NULL;
+    run->id=0;
   }
 }
 
-static void run_load_if_new_lib(struct run*run)
+static void run_load(struct run *run)
 {
-  struct stat attr;
-  if(stat(RUN_LIBRARY,&attr)!=0){
-    printf("run library doesn't exist.\n");
-    return;
-  }
-  if (run->id == attr.st_ino) { // note: id is initially 0 and the
-				 // file should therefore be loaded at
-				 // first call
-    printf("file didn't change\n");
-    return;
-  }
-  if(run->handle){
-    run->api.unload(run->state);
-    dlclose(run->handle);
-  }
-  void *handle = dlopen(RUN_LIBRARY,RTLD_NOW);
-  if(!handle){
-    run->handle = NULL;
-    run->id = 0;
-    printf("error during dlopen.\n");
-    return;
-  }
-  run->handle = handle;
-  run->id = attr.st_ino;
-  const struct run_api *api =
-    dlsym(run->handle,"RUN_API");
-  if(api == NULL){
-    dlclose(run->handle);
-    run->handle = NULL;
-    run->id = 0;
-    printf("error during dlsym of api struct.\n");
-    return ;
-  }
-
-  run->api = *api;
-  if(run->state == NULL)
-    run->state = run->api.init();
-  run->api.reload(run->state);
+    struct stat attr;
+    if ((stat(RUN_LIBRARY, &attr) == 0) && (run->id != attr.st_ino)) {
+        if (run->handle) {
+            run->api.unload(run->state);
+            dlclose(run->handle);
+        }
+        void *handle = dlopen(RUN_LIBRARY, RTLD_NOW);
+        if (handle) {
+            run->handle = handle;
+            run->id = attr.st_ino;
+            const struct run_api *api = dlsym(run->handle, "RUN_API");
+            if (api != NULL) {
+                run->api = *api;
+                if (run->state == NULL)
+                    run->state = run->api.init();
+                run->api.reload(run->state);
+            } else {
+                dlclose(run->handle);
+                run->handle = NULL;
+                run->id = 0;
+            }
+        } else {
+            run->handle = NULL;
+            run->id = 0;
+        }
+    }
 }
+
+
+/* static void run_load_if_new_lib(struct run*run) */
+/* { */
+/*   struct stat attr; */
+/*   d(printf("trying to load library\n")); */
+/*   if(stat(RUN_LIBRARY,&attr)!=0){ */
+/*     d(printf("RUN_LIBRARY doesn't exist.\n")); */
+/*     return; */
+/*   } */
+/*   d(printf("check if inode was modified\n")); */
+/*   if (run->id == attr.st_ino) { // note: id is initially 0 and the */
+/* 				 // file should therefore be loaded at */
+/* 				 // first call */
+/*     d(printf("file didn't change\n")); */
+/*     return; */
+/*   } */
+/*   if(run->handle){ */
+/*     d(printf("library was already open, closing it.\n")); */
+/*     run->api.unload(run->state); */
+/*     dlclose(run->handle); */
+/*   } */
+/*   d(printf("dlopen library\n")); */
+/*   void *handle = dlopen(RUN_LIBRARY,RTLD_NOW); */
+/*   if(!handle){ */
+/*     run->handle = NULL; */
+/*     run->id = 0; */
+/*     d(printf("error during dlopen.\n")); */
+/*     return; */
+/*   } */
+/*   run->handle = handle; */
+/*   run->id = attr.st_ino; */
+/*   d(printf("load the struct\n")); */
+/*   const struct run_api *api = */
+/*     dlsym(run->handle,"RUN_API"); */
+/*   if(api == NULL){ */
+/*     dlclose(run->handle); */
+/*     run->handle = NULL; */
+/*     run->id = 0; */
+/*     d(printf("error during dlsym of api struct.\n")); */
+/*     return ; */
+/*   } */
+/*   d(printf("copy the data\n")); */
+/*   run->api = *api; */
+/*   if(run->state == NULL){ */
+/*     d(printf("set state by calling init\n")); */
+/*     if(run->api.init) */
+/*       run->state = run->api.init(); */
+/*     else { */
+/*       d(printf("init isn't defined\n")); */
+/*       return; */
+/*     } */
+/*   } */
+/*   if(run->api.reload && run->state){ */
+/*     d(printf("call reload\n")); */
+/*     run->api.reload(run->state); */
+/*   } */
+/* } */
 
 int main(void)
 {
   struct run run = {0};
   for(;;){
-    run_load_if_new_lib(&run);
-    if(run.handle)
-      if(!run.api.step(run.state))
+    d(printf("try to reload\n"));
+    //run_load_if_new_lib(&run);
+    run_load(&run);
+    d(printf("call step\n"));
+    if(run.handle){
+      //      d(printf("step is pointer to this address: %lx %lx %lx\n", run.api, run.api.step, run.state));
+      if(run.api.step==NULL){
+	d(printf("step is undefined\n"));
 	break;
+      }
+      if(run.state==NULL){
+	d(printf("state is undefined\n"));
+	break;
+      }
+      
+      {
+	int ret = run.api.step(run.state);
+	d(printf("step returned %d\n",ret));
+	if(!ret)
+	  break;
+      }
+    }
+    d(printf("sleep\n"));
     usleep(100000);
   }
+  d(printf("unload\n"));
   run_unload(&run);
   return 0;
 }
--- makefile
+++ makefile
@@ -1,13 +1,16 @@
-CFLAGS=-ggdb -Wall -Wextra -O2 -march=native
-
+CFLAGS=-ggdb -Wall -Wextra -O0 -march=native -pedantic  -fPIC -D_BSD_SOURCE -std=c99
+LDLIBS=-ldl
 
 all: main librun.so
 
 main: main.c api.h
-	$(CC) $(CFLAGS) -ldl -o main main.c
+	$(CC) $(CFLAGS) -o $@ $< $(LDLIBS)
 
 librun.so: run.c api.h
-	$(CC) $(CFLAGS) $(LDFLAGS) -o librun.so -shared -fPIC run.c -nodefaultlibs
+	$(CC) $(CFLAGS) -shared  $(LDFLAGS) -o $@  $< $(LDLIBS)
 
 clean:
 	rm main librun.so
+
+test : main libgame.so
+	./$<
